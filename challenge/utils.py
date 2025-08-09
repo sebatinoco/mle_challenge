97d5cb0908a4acce8829bfc0bf4672347d1a7b8e
Sebastian Tinoco <stinoco@fen.uchile.cl>
2025-08-08 23:31:19 -0400

    feat: added utils.py to challenge folder

---
+++ challenge/utils.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+def get_min_diff(data):
+    fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+    fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+    min_diff = ((fecha_o - fecha_i).total_seconds())/60
+    return min_diff
